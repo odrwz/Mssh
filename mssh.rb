dff21db2cfff1ade64071983a8baadf145c184a1
Temp User <temp@example.com>
2026-02-21 14:03:20 +0800

    refactor: use 'q' for quit/back/cancel, align prompts, remove examples

--- mssh.rb
+++ mssh.rb
@@ -2,7 +2,7 @@ class Mssh < Formula
   desc "SSH Host & Key Manager CLI"
   homepage "https://github.com/odrwz/CLImssh"
   url "https://raw.githubusercontent.com/odrwz/CLImssh/main/climssh"
-  # sha256 "<run: sha256sum climssh> and paste here after tagging"
+  sha256 "1dfa6ddd29c441fc242a109f36a49dbcfffcaf92a5714f289cb7163f18b23849"
   version "2.0.0"
 
   def install
